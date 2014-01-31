c412d83487a445f6872c69d269b728d7b000353f
malju <mallor.koor@khk.ee>
2014-02-01 01:20:41 +0200

    Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,7 +1,7 @@
 <?foreach( $posts as $post):?>
 <div class="container">
     <div class="col-md-12">
-        <h1><?=$post['post_subject']?></h1>
+        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
         <p><?=$post['post_text']?></p>
         <div>
             <span class="badge">Posted <?=$post['post_created']?></span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
---
+++ views/posts/posts_view.php
@@ -0,0 +1,12 @@
+
+<div class="container">
+    <div class="col-md-12">
+        <h1><?=$post['post_subject']?></h1>
+        <p><?=$post['post_text']?></p>
+        <div>
+            <span class="badge">Posted <?=$post['post_created']?> by <?=$post['username']?></span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
+                <span class="label label-danger">Danger</span></div>
+        </div>
+        <hr>
+    </div>
+</div>
