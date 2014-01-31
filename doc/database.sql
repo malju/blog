3242b11e60d4dd53d77d78b7c0ca90756f0b0361
malju <mallor.koor@khk.ee>
2014-02-01 00:54:21 +0200

    Add posts listing functionality)

--- doc/database.sql
+++ doc/database.sql
@@ -1,24 +1,44 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2013 at 10:20 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Jan 31, 2014 at 11:52 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
 
 SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Database: `blog`
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- --------------------------------------------------------
 
 --
--- Database: `halo`
+-- Table structure for table `post`
 --
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `post_subject` varchar(255) NOT NULL,
+  `post_text` text NOT NULL,
+  `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
+(1, 'Facebook supleb reklaamirahas', 'Lennukaid majandustulemusi näidanud Facebook on reklaamijad kenasti konksu otsa saanud ning korjab neilt raha nii mis mühiseb. \r\n\r\nNagu ütles Financial Timesile SunTrust Robinson Humphrey analüütik Robert Peck, on eile 82-protsendilise kasumikasvu ette kandnud sotsiaalvõrgustikul õnnestunud panna ettevõtjad üha kallimat reklaamipinda ostma, kuna nood näevad selles tugevat tulu.\r\n\r\nAlgatuseks meelitas Facebook suured brändid ligi, pakkudes neile võimalust reklaamida end oma üüratule kliendibaasile tasuta – kõik mis tuli teha, oli luua oma lehekülg ja hakata inimestele «meeldima». Siis sai rõõmsalt reklaamida, selleks sentigi kulutamata.\r\n\r\nNüüd, kus reklaamijaid tungleb hulgana – Facebooki mullune kogukäive kerkis 7,9 miljardi dollari kõrgustesse – ning neil on soov oma reklaamidega üha täpsemalt märki tabada, tuleb nähtavuse eest tasuda.\r\n\r\nGlobaalsel reklaamiturul, USA digireklaamiturul ja ülemaailmsel mobiilreklaamiturul jääb Facebook nüüd maha veel üksnes Google’ist, näitavad eMarketeri andmed. Samas kinnitab Socialbakersi analüütik Jan Rezab, et mõnes mõttes on Facebook turundajate meelest etem.\r\n\r\n«Google eeldab sirvimise alusel, et sulle meeldib see või too. Facebook teab, sest ta on sinuga otseühenduses,» selgitas Rezab – Facebook ei ole üksnes kursis sellega, mis leheküljed sulle meeldivad, vaid on samuti kursis, millist sisu sa agaralt tarbid (seda näiteks kommenteerides). ', '2014-01-31 22:30:46', 1),
+(2, 'Apple veel iPodi ei hülga', 'Kuigi iPodide müük on drastiliselt langenud, on turule oodata vähemalt veel ühte põlvkonda Apple´i muusikapleiereid, kirjutab Techradar.\r\n\r\nApple tutvustas oma iPodi pleierit 2001. aastal, kuid peale 2008. aastat on müügitulemused nutitelefonide turu plahvatusliku kasvu tõttu kokku kuivanud.\r\n\r\nEelmise aasta viimases kvartalis langes iPodide müük aastases võrdluses 52 protsenti 6 miljoni seadmeni.\r\n\r\nSamas ei plaani Apple ilmselt veel lähiajal muusikapleierite tootmist lõpetada. Nimelt otsib ettevõte iPodi üksusesse uusi toote- ja müügijuhte, mis Techradari hinnangul tähendab, et oodata on veel vähemalt ühte uut põlvkonda Apple´i muusikapleiereid.', '2014-01-31 22:30:46', 1);
 
 -- --------------------------------------------------------
 
@@ -41,8 +61,14 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
-SET FOREIGN_KEY_CHECKS=1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+SET FOREIGN_KEY_CHECKS=1;
